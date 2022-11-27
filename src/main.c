5e7fca59fbfc4f5fee973f51896b53b000ef9928
PaulaSousa2014 <110301198+PaulaSousa2014@users.noreply.github.com>
2022-11-27 08:22:15 +0100

    Enunciado del programa

--- src/main.c
+++ src/main.c
@@ -1,8 +1,23 @@
-/*	Autor: XXX		Fecha de creación: XXX  */
+/*	Autor: Paula Martins de Sousa		Fecha de creación: 27/11/2022  */
 
 /*
 **	ENUNCIADO DEL PROGRAMA
-**	Se desea ..........
+**	Se desea hacer un programa en C para poder jugar al DOBLEACIERTO. 
+Este juego trata de elegir 2 números del 0 al 9 a través de un procedimiento 
+(“combinacionUsuario”) que serán pedidos a través de la función 
+(“pideNumEntreRango”). Es decir, dentro de combinacionUsuario hay dos llamadas 
+a pideNumEntreRango.
+Posteriormente, el sistema genera aleatoriamente (rand) la combinación ganadora de 
+2 números (también entre 0 y 9) a través de un procedimiento llamado 
+“combinacionGanadora”. 
+Finalmente, hay que crear una función que dados los valores del usuario y los 
+aleatorios, devuelva al main cuántos aciertos ha tenido el usuario
+(“calculaAciertos”). 
+Los números del usuario NO se pueden repetir ni los números de la combinación 
+ganadora no se pueden repetir. 
+Para calcular el número de aciertos, no es necesario que los números estén en la misma 
+posición, simplemente es necesario que sean iguales.
+
 **
 */
 
