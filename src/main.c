80f7703f51e291f321169fa92ece04b65b9fbaee
PaulaSousa2014 <110301198+PaulaSousa2014@users.noreply.github.com>
2022-11-27 08:57:26 +0100

    Combinacion ganadora (Rand())

--- src/main.c
+++ src/main.c
@@ -2,54 +2,52 @@
 
 /*
 **	ENUNCIADO DEL PROGRAMA
-**	Se desea hacer un programa en C para poder jugar al DOBLEACIERTO. 
-Este juego trata de elegir 2 números del 0 al 9 a través de un procedimiento 
-(“combinacionUsuario”) que serán pedidos a través de la función 
-(“pideNumEntreRango”). Es decir, dentro de combinacionUsuario hay dos llamadas 
+**	Se desea hacer un programa en C para poder jugar al DOBLEACIERTO.
+Este juego trata de elegir 2 números del 0 al 9 a través de un procedimiento
+(“combinacionUsuario”) que serán pedidos a través de la función
+(“pideNumEntreRango”). Es decir, dentro de combinacionUsuario hay dos llamadas
 a pideNumEntreRango.
-Posteriormente, el sistema genera aleatoriamente (rand) la combinación ganadora de 
-2 números (también entre 0 y 9) a través de un procedimiento llamado 
-“combinacionGanadora”. 
-Finalmente, hay que crear una función que dados los valores del usuario y los 
+Posteriormente, el sistema genera aleatoriamente (rand) la combinación ganadora de
+2 números (también entre 0 y 9) a través de un procedimiento llamado
+“combinacionGanadora”.
+Finalmente, hay que crear una función que dados los valores del usuario y los
 aleatorios, devuelva al main cuántos aciertos ha tenido el usuario
-(“calculaAciertos”). 
-Los números del usuario NO se pueden repetir ni los números de la combinación 
-ganadora no se pueden repetir. 
-Para calcular el número de aciertos, no es necesario que los números estén en la misma 
+(“calculaAciertos”).
+Los números del usuario NO se pueden repetir ni los números de la combinación
+ganadora no se pueden repetir.
+Para calcular el número de aciertos, no es necesario que los números estén en la misma
 posición, simplemente es necesario que sean iguales.
 
 **
 */
 
 #include <stdio.h>
-void combinacionUsuario(int []);
+void combinacionUsuario(int[]);
 int pideNumEntreRango(int, int);
-// void combinacionGanadora();
+void combinacionGanadora(int[]);
 // int calculaAciertos(int [], int []);
 
-int main(){
-int vUser[2];
+int main()
+{
+	int vUser[2];
+	int vRand[2];
 
+	printf("BIENVENIDOS AL PROGRAMA DE LOTERIA DOBLE ACIERTO:\n");
+	combinacionUsuario(vUser);
+	combinacionGanadora(vRand);
 
-printf("BIENVENIDOS AL PROGRAMA DE LOTERIA DOBLE ACIERTO:\n");
-combinacionUsuario(vUser);
-
-
-	
 	getch();
-	
+
 	return 0;
 }
 
 void combinacionUsuario(int v[])
 {
 
-for (int i=0;i<2;i++)
-{
-v[i]= pideNumEntreRango(0,9);
-
-
-}
+	for (int i = 0; i < 2; i++)
+	{
+		v[i] = pideNumEntreRango(0, 9);
+	}
 }
 pideNumEntreRango(min, max)
 {
@@ -62,4 +60,11 @@ pideNumEntreRango(min, max)
 	} while (num < 0 || num > 9);
 
 	return num;
+}
+void combinacionGanadora(int v[])
+{
+for (int i = 0; i < 2; i++)
+	{
+		v[i] = rand() %10;
+	}
 }
